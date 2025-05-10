12dcf5dbd4e45e3bb7ef0cc787f3c402ae3c6db4
Pablo Lopez <pablo.lpzh@gmail.com>
2025-05-10 11:19:19 -0400

    Se realiza la interfaz visual de postulacionAyudante y se agrega un icono de notificaciones en navbar

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('buscador/', views.buscador_view, name='buscador')
+    path('buscador/', views.buscador_view, name='buscador'),
+    path('postulacionAyudante/', views.postulacionAyudante_view, name='postulacionAyudante'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,6 @@ def inicio(request):
 
 def buscador_view(request):
     return render(request, 'buscador.html')
+
+def postulacionAyudante_view(request):
+    return render(request, 'postulacionAyudante.html')
