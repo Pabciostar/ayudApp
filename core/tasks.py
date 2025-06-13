529cc997132db5f20d0c72717c0073b1da414ac5
Pablo Lopez <pablo.lpzh@gmail.com>
2025-06-13 17:46:44 -0400

    configuración de django-background-taks para activar envío de notificaciones periodicamente en segundo plano y automáticamente

---
+++ core/tasks.py
@@ -0,0 +1,15 @@
+from background_task import background
+from core.utils import verificar_y_enviar_recordatorios
+from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@background(schedule=5, remove_existing_tasks=True)
+def tarea_enviar_recordatorios():
+    logger.info("Recordatorio enviado: %s", datetime.now())
+    verificar_y_enviar_recordatorios()
+
+    # Reprogramarse para ejecutarse nuevamente en 1 minuto
+    tarea_enviar_recordatorios(schedule=10)
